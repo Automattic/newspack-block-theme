fb4db4fb7cf3b7600d596e93a60a1ad42ef2d203
Derrick Koo <derrick.koo@automattic.com>
2023-04-18 17:15:09 -0600

    refactor: namespace WP functions per Newspack code style

--- includes/class-core.php
+++ includes/class-core.php
@@ -67,15 +67,15 @@ final class Core {
 	public static function theme_styles() {
 
 		// Register theme stylesheet.
-		wp_register_style(
+		\wp_register_style(
 			'newspack_block_theme-style',
-			get_stylesheet_directory_uri() . '/style.css',
+			\get_stylesheet_directory_uri() . '/style.css',
 			array(),
-			wp_get_theme()->get( 'Version' )
+			\wp_get_theme()->get( 'Version' )
 		);
 
 		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'newspack_block_theme-style' );
+		\wp_enqueue_style( 'newspack_block_theme-style' );
 	}
 }
 
