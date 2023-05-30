26ffe488cf929adad4d20fde0a74d4e5704f443c
Laurel Fulford <laurel.fulford@automattic.com>
2023-05-30 09:53:57 -0700

    feat: building out 404 page

--- patterns/404.php
+++ patterns/404.php
@@ -8,11 +8,11 @@
  */
 
 ?>
-
-<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"x-large"} -->
-<h1 class="has-text-align-center has-x-large-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'newspack-block-theme' ); ?></h1>
+<!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center">Page not found.</h1>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php echo esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'newspack-block-theme' ); ?></p>
+<!-- wp:paragraph {"align":"center","fontSize":"x-large"} -->
+<p class="has-text-align-center has-x-large-font-size">It looks like nothing was found at this location. Maybe try a search?</p>
 <!-- /wp:paragraph -->
+
