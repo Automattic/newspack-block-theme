5e89534b21517d8cbc59575eb05ce4494c7d0ffe
Thomas Guillot <thomasguillot@users.noreply.github.com>
2024-05-10 10:54:41 +0100

    feat: update search templates (#137)

--- patterns/404.php
+++ patterns/404.php
@@ -1,4 +1,5 @@
 <?php
+// phpcs:ignoreFile
 /**
  * Title: A 404 page
  * Slug: newspack-block-theme/404
@@ -8,11 +9,11 @@
  */
 
 ?>
-<!-- wp:heading {"textAlign":"center","level":1} -->
-<h1 class="wp-block-heading has-text-align-center">Page not found.</h1>
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php esc_html_e( 'Page not found', 'newspack-block-theme' ); ?></h1>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"center","fontSize":"x-large"} -->
-<p class="has-text-align-center has-x-large-font-size"><?php esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'newspack-block-theme' ); ?></p>
+<!-- wp:paragraph {"fontSize":"x-large"} -->
+<p class="has-x-large-font-size"><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'newspack-block-theme' ); ?></p>
 <!-- /wp:paragraph -->
 
--- patterns/hidden-no-results-content.php
+++ patterns/hidden-no-results-content.php
@@ -1,4 +1,5 @@
 <?php
+// phpcs:ignoreFile
 /**
  * Title: Hidden No Results Content
  * Slug: newspack-block-theme/hidden-no-results-content
@@ -9,21 +10,17 @@
 
 ?>
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:spacer {"height":"var:preset|spacing|50"} -->
-<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
+<div class="wp-block-group">
 
 <!-- wp:paragraph {"fontSize":"x-large"} -->
 <p class="has-x-large-font-size"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'newspack-block-theme' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:spacer {"height":"var:preset|spacing|70"} -->
-<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:heading {"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}},"fontSize":"x-large"} -->
-<h2 class="wp-block-heading alignwide has-x-large-font-size" style="margin-bottom:var(--wp--preset--spacing--50)">Latest articles</h2>
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|80","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:0"><!-- wp:heading {"textAlign":"left","level":3,"align":"wide"} -->
+<h3 class="wp-block-heading alignwide has-text-align-left"><?php esc_html_e( 'Latest articles', 'newspack-block-theme' ); ?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:newspack-blocks/homepage-articles {"showAvatar":false,"postLayout":"grid","columns":4,"postsToShow":4,"typeScale":3,"align":"wide"} /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
