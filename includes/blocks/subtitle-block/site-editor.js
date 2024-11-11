2baa2251d8966ee6ccd17924eaedd9e13c2ecf71
Adam Cassis <adam.cassis@automattic.com>
2024-11-11 10:58:38 +0100

    fix: placeholder when editing a template

--- includes/blocks/subtitle-block/site-editor.js
+++ includes/blocks/subtitle-block/site-editor.js
@@ -12,7 +12,10 @@ import blockData from './block.json';
 
 const EditComponent = ( { context: { postType, postId } } ) => {
 	const [ postMeta = {} ] = useEntityProp( 'postType', postType, 'meta', postId );
-	return postMeta[ newspack_block_theme_subtitle_block.post_meta_name ] || '';
+	return (
+		postMeta[ newspack_block_theme_subtitle_block.post_meta_name ] ||
+		__( 'Article subtitle', 'newspack-block-theme' )
+	);
 };
 
 blockData = {
