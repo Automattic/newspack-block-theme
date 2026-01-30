b06ee804d6a491582aab85b221e4c60063ce021c
Laurel Fulford <laurel.fulford@automattic.com>
2026-01-30 10:33:37 -0800

    feat: add My Account Button block to header pattern 5

--- patterns/header/header-desktop-style-5.php
+++ patterns/header/header-desktop-style-5.php
@@ -35,6 +35,10 @@
 		<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 		<div class="wp-block-group alignwide">
 			<!-- wp:site-logo {"width":256} /-->
+
+			<?php if ( class_exists( 'Newspack\Reader_Activation' ) && \Newspack\Reader_Activation::is_enabled() ) : ?>
+				<!-- wp:newspack/my-account-button {"lock":{"move":true,"remove":false},"className":"has-small-size","fontSize":"x-small"} /-->
+			<?php endif; ?>
 		</div>
 		<!-- /wp:group -->
 	</div>
