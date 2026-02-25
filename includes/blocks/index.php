8801fb1d152b0fda41ce2df111aae92e05b9b0c1
Rasmy Nguyen <rtnguyen87@gmail.com>
2026-02-25 17:21:40 -0500

    fix: remove block

--- includes/blocks/index.php
+++ includes/blocks/index.php
@@ -10,4 +10,3 @@ namespace Newspack_Block_Theme;
 defined( 'ABSPATH' ) || exit;
 
 require_once NEWSPACK_BLOCK_THEME_FILE_PATH . '/includes/blocks/subtitle-block/class-subtitle-block.php';
-require_once NEWSPACK_BLOCK_THEME_FILE_PATH . '/includes/blocks/featured-image-caption/class-featured-image-caption.php';
